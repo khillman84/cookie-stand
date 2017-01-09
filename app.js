8639aeeef8e5d223e42df6a58019178e83740986
Kyle Hillman <khillman84@gmail.com>
2017-01-09 15:18:56 -0800

    added store locations as objects in app.js

--- app.js
+++ app.js
@@ -1,5 +1,34 @@
 'use strict';
 
-var test = 'testing';
+//List of current store locations stored as objects
+var store1 = {
+  name: '1st and Pike',
+  address: '1234 1st Ave SE Seattle WA 98121',
+  phone: '(206) 867-5309',
+};
 
-console.log(test);
+var store2 = {
+  name: 'Seatac Airport',
+  address: '1708 International Blvd Seattle WA 98158',
+  phone: '(206) 867-5308',
+};
+
+var store3 = {
+  name: 'Seattle Center',
+  address: '305 Harrison St Seattle WA 98109',
+  phone: '(206) 867-5307',
+};
+
+var store4 = {
+  name: 'Capital Hill',
+  address: '210 E Pike St Seattle WA 98102',
+  phone: '(206) 867-5306',
+};
+
+var store5 = {
+  name: 'Alki',
+  address: '1702 Alki Ave SW Seattle WA 98116',
+  phone: '(206) 867-5305',
+};
+
+//Populate list of store locations
