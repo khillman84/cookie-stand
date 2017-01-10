c236a47054c842ada0c4b0de82e469506dc1047f
Kyle Hillman <khillman84@gmail.com>
2017-01-09 19:38:52 -0800

    added final two stores to the sales.html file

--- app.js
+++ app.js
@@ -101,3 +101,69 @@ var seattleCenter = {
   }
 };
 seattleCenter.print();
+
+//store information stored as an object
+var capitalHill = {
+  minHourlyCust: 23,
+  maxHourlyCust: 65,
+  avgCookies: 6.3,
+  location: [],
+  customers: function() {   //average number of customers per hour
+    var people = Math.floor(Math.random() * (this.maxHourlyCust - this.minHourlyCust) + this.minHourlyCust);
+    console.log(people);
+    return people;
+  },
+  cookiesPerHour: function() {    //amount of cookies sold per hour
+    for(var i = 0; i < hours.length; i ++){
+      var amount = Math.floor(this.customers() * this.avgCookies);
+      console.log(amount);
+      this.location.push(amount);
+    }
+    return this.location;
+  },
+
+  print: function() {   //print to sales.html
+    var businessList = document.getElementById('capitalHill');
+    this.cookiesPerHour();
+    console.log(this.location);
+    for(var i = 0; i < this.location.length; i ++) {
+      var listEl = document.createElement('li');
+      listEl.textContent = hours[i] + this.location[i];
+      businessList.appendChild(listEl);
+    }
+  }
+};
+capitalHill.print();
+
+//store information stored as an object
+var alki = {
+  minHourlyCust: 23,
+  maxHourlyCust: 65,
+  avgCookies: 6.3,
+  location: [],
+  customers: function() {   //average number of customers per hour
+    var people = Math.floor(Math.random() * (this.maxHourlyCust - this.minHourlyCust) + this.minHourlyCust);
+    console.log(people);
+    return people;
+  },
+  cookiesPerHour: function() {    //amount of cookies sold per hour
+    for(var i = 0; i < hours.length; i ++){
+      var amount = Math.floor(this.customers() * this.avgCookies);
+      console.log(amount);
+      this.location.push(amount);
+    }
+    return this.location;
+  },
+
+  print: function() {   //print to sales.html
+    var businessList = document.getElementById('alki');
+    this.cookiesPerHour();
+    console.log(this.location);
+    for(var i = 0; i < this.location.length; i ++) {
+      var listEl = document.createElement('li');
+      listEl.textContent = hours[i] + this.location[i];
+      businessList.appendChild(listEl);
+    }
+  }
+};
+alki.print();
