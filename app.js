8808b76626eb2ef583ae388ee066f98e89b521b3
Kyle Hillman <khillman84@gmail.com>
2017-01-10 17:08:20 -0800

    added hours array into location array to build out table

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 'use strict';
 
-//global
+//global variables
 var hours = ['6am: ', '7am: ', '8am: ', '9am: ', '10am: ', '11am: ', '12pm: ', '1pm: ', '2pm: ', '3pm: ', '4pm: ', '5pm: ', '6pm: ', '7pm: ', '8pm: ', 'Total: '];
 
 //Store object
@@ -8,38 +8,37 @@ function Store(minHourlyCust, maxHourlyCust, avgCookies){
   this.minHourlyCust = minHourlyCust;
   this.maxHourlyCust = maxHourlyCust;
   this.avgCookies = avgCookies;
-  this.location = [];
+  this.location = [hours, []];
 };
 
 //methods to run when Store object is called
 Store.prototype.cookiesPerHour = function() {   //calculate the amount of cookies sold per hour
   for(var i = 0; i < hours.length - 1; i ++){
     var people = Math.round(Math.random() * (this.maxHourlyCust - this.minHourlyCust) + this.minHourlyCust);
-    console.log(people);
+    console.log('People: ' + people);
     var amount = Math.round(people * this.avgCookies);
-    console.log(amount);
-    this.location.push(amount);
+    console.log('Amount sold: ' + amount);
+    this.location[1].push(amount);
   }
   return this.location;
 };
 
 Store.prototype.amount = function() {   //add all the total amount of cookies sold
   var sum = 0;
-  for(var j = 0; j < this.location.length; j ++){
-    sum += this.location[j];
+  for(var j = 0; j < this.location[1].length; j ++){
+    sum += this.location[1][j];
   }
-  this.location.push(sum);
-  console.log(sum);
+  this.location[1].push(sum);
+  console.log(this.location[1]);
 };
 
 Store.prototype.print = function() {    //print the cookies sold as a table in html
-  var businessList = document.getElementById('pike');
+  var businessList = document.getElementById('table');
   this.cookiesPerHour();
   this.amount();
-  console.log(this.location);
-  for(var i = 0; i < this.location.length; i ++) {
+  for(var i = 0; i < this.location[1].length; i ++) {
     var listEl = document.createElement('li');
-    listEl.textContent = hours[i] + this.location[i];
+    listEl.textContent = hours[i] + this.location[1][i];
     businessList.appendChild(listEl);
   }
 };
