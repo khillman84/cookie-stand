06baf3703c49738d2363b37414f396f63bc580bb
Kyle Hillman <khillman84@gmail.com>
2017-01-12 22:00:05 -0800

    added JS totals to the bottom of the table

--- app.js
+++ app.js
@@ -2,16 +2,41 @@
 
 //global variables
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm', 'Total'];
+var stores = [];
+var total = [];
 
 //global functions
-function hourHeader() {
+function hourHeader() {  //print the hours of operation
   var tableEl = document.getElementById('hours-row');
   for (var i = 0; i < hours.length; i ++) {
     var rowData = document.createElement('td');
     rowData.textContent = hours[i];
     tableEl.appendChild(rowData);
   }
-  console.log();
+};
+
+function getHourTotal() { //sum the amount of cookies sold each hour
+  total = [];
+  for (var i = 0; i < hours.length; i ++){
+    var hour = 0;
+    for (var j = 0; j < stores.length; j++){
+      var storeCount = stores[j].location[i];
+      hour = hour + storeCount;
+    }
+    total.push(hour);
+  }
+};
+
+function footerHeader() { //print the total cookies sold each hour
+  var tableEl = document.getElementById('table');
+  var blankHead = document.createElement('hd');
+  blankHead.textContent = 'Totals';
+  tableEl.appendChild(blankHead);
+  for (var i = 0; i < total.length; i ++) {
+    var rowData = document.createElement('td');
+    rowData.textContent = total[i];
+    tableEl.appendChild(rowData);
+  }
 };
 
 //Store object
@@ -25,23 +50,20 @@ function Store(minHourlyCust, maxHourlyCust, avgCookies, name){
 
 //methods
 Store.prototype.cookiesPerHour = function() {   //calculate the amount of cookies sold per hour
-  for(var i = 0; i < hours.length - 1; i ++){
+  for (var i = 0; i < hours.length - 1; i++) {
     var people = Math.round(Math.random() * (this.maxHourlyCust - this.minHourlyCust) + this.minHourlyCust);
-    console.log('People: ' + people);
     var amount = Math.round(people * this.avgCookies);
-    console.log('Amount sold: ' + amount);
     this.location.push(amount);
   }
-  return this.location;
 };
 
 Store.prototype.amount = function() {   //add the total amount of cookies sold
   var sum = 0;
-  for(var j = 0; j < this.location.length; j ++){
-    sum += this.location[j];
+  for(var i = 0; i < this.location.length; i ++){
+    sum += this.location[i];
   }
   this.location.push(sum);
-  console.log(this.location);
+  // console.log(this.location);
 };
 
 Store.prototype.storeName = function() {    //push the store name into the this.location array
@@ -49,29 +71,32 @@ Store.prototype.storeName = function() {    //push the store name into the this.
 };
 
 Store.prototype.cookieTotals = function() {   //print the information into a table on the sales.html page
-  var tableEl = document.getElementById('table');
   var arr = this.location;
+  var tableEl = document.getElementById('table');
+  var newRow = document.createElement('tr');
+  var nameEl = document.createElement('th');
+  nameEl.textContent = this.name;
+  newRow.appendChild(nameEl);
   for (var i = 0; i < arr.length; i ++) {
     var rowData = document.createElement('td');
     rowData.textContent = arr[i];
-    var newRow = document.createElement('tr');
-    tableEl.appendChild(rowData);
-    console.log('Inside cookie totals: ' + arr[i]);
+    newRow.appendChild(rowData);
+    // console.log('Inside cookie totals: ' + arr[i]);
   }
+
   tableEl.appendChild(newRow);
 };
 
 Store.prototype.print = function() {    //call the methods in proper order
   this.cookiesPerHour();
   this.amount();
-  this.storeName();
   this.cookieTotals();
 };
 
 //event listeners
-var formEl = document.getElementById('store-info');
+var formEl = document.getElementById('store-info');   //When form is submitted, populate table with new info
 
-formEl.addEventListener('submit',function(event){
+formEl.addEventListener('submit', function(event){
   event.preventDefault();
   event.stopPropagation();
 
@@ -86,10 +111,15 @@ formEl.addEventListener('submit',function(event){
 
 //created stores
 var firstAndPike = new Store(23, 65, 6.3, 'First and Pike');
+stores.push(firstAndPike);
 var seatac = new Store(2, 23, 1.2, 'Seatac');
+stores.push(seatac);
 var seattleCenter = new Store(11, 38, 3.7, 'Seattle Center');
+stores.push(seattleCenter);
 var capitalHill = new Store(20, 38, 2.3, 'Capital Hill');
+stores.push(capitalHill);
 var alki = new Store(2, 16, 4.6, 'Alki');
+stores.push(alki);
 
 //call the stores to print
 hourHeader();
@@ -98,3 +128,5 @@ seatac.print();
 seattleCenter.print();
 capitalHill.print();
 alki.print();
+getHourTotal();
+footerHeader();
